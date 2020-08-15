5e2ed876d0b55adff9437cf68627b85537e335cb
ashwath <ashwath.acharya@paymentz.com>
2020-08-15 18:16:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import faker from "faker";
+import "./App.css";
+import TopBar from "./components/TopBar";
+import Container from "./components/Container";
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+      <Container />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TopBar/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Logo from "./Logo";
+
+export default function TopBar() {
+  return <div className="tb">
+      <Logo />
+  </div>;
+}
